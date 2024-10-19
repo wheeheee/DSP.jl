f7515ad5c640df6ef82f2587dd6e4597b72b4c13
wheeheee <104880306+wheeheee@users.noreply.github.com>
2024-10-19 08:49:26 +0800

    typos

--- src/Filters/design.jl
+++ src/Filters/design.jl
@@ -384,7 +384,7 @@ function transform_prototype(ftype::Bandstop, proto::ZeroPoleGain{:s})
         newp[2i] = b + pm
     end
 
-    # Any emaining poles/zeros are real and not cancelled
+    # Any remaining poles/zeros are real and not cancelled
     npm = sqrt(-complex(ftype.w2 * ftype.w1))
     for (n, newc) in ((np, newp), (nz, newz))
         for i = n+1:npairs
@@ -453,8 +453,8 @@ function bilinear(f::ZeroPoleGain{:s,Z,P,K}, fs::Real) where {Z,P,K}
     ztype = typeof(0 + zero(Z)/fs)
     z = fill(convert(ztype, -1), max(length(f.p), length(f.z)))
 
-    ptype = typeof(0 + zero(P)/fs)
-    p = Vector{typeof(zero(P)/fs)}(undef, length(f.p))
+    ptype = typeof(0 + zero(P) / fs)
+    p = Vector{ptype}(undef, length(f.p))
 
     num = one(one(fs) - one(Z))
     for i = 1:length(f.z)
@@ -509,7 +509,8 @@ function iirnotch(w::Real, bandwidth::Real; fs=2)
     b = 1 / (1 + tanpi(bandwidth / 2))
     # Eq. 8.2.22
     cosw0 = cospi(w)
-    Biquad(b, -2b*cosw0, b, -2b*cosw0, 2b-1)
+    b1 = -2b * cosw0
+    Biquad(b, b1, b, b1, 2b-1)
 end
 
 #
